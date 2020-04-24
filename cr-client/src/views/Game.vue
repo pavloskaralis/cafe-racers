92dcb3f253efe4c81975c59b8007ea6e97121dbf
pavloskaralis <52301933+pavloskaralis@users.noreply.github.com>
2020-04-24 16:58:36 -0400

    set dynamic component

--- cr-client/src/views/Game.vue
+++ cr-client/src/views/Game.vue
@@ -6,91 +6,37 @@
           <div class="table"></div>
           <div class="table-extension"></div>
       </div>
-      <div></div>
-
+      <component :is="activeView"/>
   </div>
 </template>
  
  
 <script lang="js">
-    // import VersusAI from "@/components/VersusAI.vue";
-    // import TwoPlayer from "@/components/TwoPlayer.vue";
+    import VersusAI from "@/components/VersusAI.vue";
+    import TwoPlayer from "@/components/TwoPlayer.vue";
 
     export default  {
         name: "game",
-        props: [],
-        mounted() {
-    
+        components: {
+          "two-player": TwoPlayer,
+          "versus-ai": VersusAI
         },
+        props: [],
         data() {
-        return {
-    
-        }
+          return {
+            activeView: ''
+          }
         },
         methods: {
-    
+          setActiveView () {
+            let path = this.$route.path; 
+            this.activeView = path === "/versus-ai" ? "versus-ai" : "two-player";
+          }
         },
-        computed: {
-    
+        mounted () {
+          this.setActiveView();
         }
     }
 </script>
  
-<style scoped lang="scss">
-  .game {
-    width: 100%;
-    height: 100%;
-  
-    .game-background {
-      position:absolute;
-      // z-index: 0;
-      background-image: url("../assets/game-background.jpg");
-      background-position: center;
-      background-size: cover;
-      background-repeat: no-repeat;
-      height: 50%;
-      width: 100%;
-      top: 0; 
-
-    }
-
-    .background-overlay {
-      position: absolute;
-      top: 0; 
-      width: 100%;
-      height: 100%;
-      background-image: linear-gradient(
-        180deg, 
-        rgba($white, 0) 20%,
-        rgba($white, .95) 50%
-      );
-    }
-
-    .table-container {
-      @extend .vertical-align;
-      position: absolute;
-      bottom: 0;
-    
-      width: 100%;
-
-
-      .table {
-        height: 25vh;
-        width: 100%;
-        background-color: $brown;
-        background-image: url("../assets/table.jpg");
-        background-position: bottom;
-        background-size: cover;
-        background-repeat: no-repeat;
-      }
-
-      .table-extension {
-        height: 33vh;
-        width: 100%;
-        background-color: $brown;
-      }
-
-    }
-    // background: red; 
-  }
-</style>
+<style src="../styles/Game.scss" scoped lang="scss"></style>
